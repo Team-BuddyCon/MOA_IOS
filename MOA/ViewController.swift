12da643309a5028ca45848727e4f5368e2c96b91
ows3090 <ows3090@naver.com>
2024-08-13 22:50:36 +0900

    init commit

---
+++ MOA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MOA
+//
+//  Created by 오원석 on 8/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
