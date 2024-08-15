8b2b333c52ea25e336d380eca7ae1a6862ea0629
ows3090 <ows3090@naver.com>
2024-08-15 15:48:54 +0900

    #1 BaseViewController, BaseViewModel 구현

---
+++ MOA/presentation/Base/BaseViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.swift
+//  MOA
+//
+//  Created by 오원석 on 8/14/24.
+//
+
+import UIKit
+import RxSwift
+
+public class BaseViewController: UIViewController {
+    let disposeBag = DisposeBag()
+}
---
+++ MOA/presentation/Base/BaseViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  BaseViewModel.swift
+//  MOA
+//
+//  Created by 오원석 on 8/14/24.
+//
+
+import Foundation
+
+public class BaseViewModel {
+    
+}
