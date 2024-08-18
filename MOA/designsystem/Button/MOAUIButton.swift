19d4880a096c1caca6d0027bc6d4673ed11d4c8c
ows3090 <ows3090@naver.com>
2024-08-18 12:08:44 +0900

    #2 색상 정의

---
+++ MOA/designsystem/Button/MOAUIButton.swift
@@ -0,0 +1,8 @@
+//
+//  MOAUIButton.swift
+//  MOA
+//
+//  Created by 오원석 on 8/18/24.
+//
+
+import Foundation
---
+++ MOA/designsystem/Color+.swift
@@ -0,0 +1,28 @@
+//
+//  Color+.swift
+//  MOA
+//
+//  Created by 오원석 on 8/17/24.
+//
+
+import UIKit
+
+extension UIColor {
+    /// 0xFFFF4E6E
+    static let pink100 = UIColor(red: 255/255, green: 78/255, blue: 110/255, alpha: 1.0)
+    
+    /// 0xFFECEDEF
+    static let grey30 = UIColor(red: 236/255, green: 237/255, blue: 239/255, alpha: 1.0)
+    
+    /// 0xFFCBCCCE
+    static let grey40 = UIColor(red: 203/255, green: 204/255, blue: 206/255, alpha: 1.0)
+    
+    /// 0xFF838486
+    static let grey60 = UIColor(red: 131/255, green: 132/255, blue: 134/255, alpha: 1.0)
+    
+    /// 0xFF6F6F71
+    static let grey70 = UIColor(red: 111/255, green: 111/255, blue: 113/255, alpha: 1.0)
+    
+    /// 0xFF2D2D2D
+    static let gry90 = UIColor(red: 45/255, green: 45/255, blue: 45/255, alpha: 1.0)
+}
