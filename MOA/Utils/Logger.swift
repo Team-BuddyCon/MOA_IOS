934c54a38962b0ba08bfa288ed34646317a3f460
ows3090 <ows3090@naver.com>
2024-10-22 22:45:54 +0900

    #21 로그 유틸리티 개발

---
+++ MOA/Utils/Logger.swift
@@ -0,0 +1,40 @@
+//
+//  Logger.swift
+//  MOA
+//
+//  Created by 오원석 on 10/20/24.
+//
+
+import Foundation
+import OSLog
+
+struct Logging {
+    private static let logger = Logger()
+    
+    static func logd(
+        _ message: String,
+        file: String = #file,
+        function: String = #function,
+        line: Int = #line
+    ) {
+        logger.debug("\(file.components(separatedBy: "/").last ?? "")(\(line)) \(function): \(message)")
+    }
+    
+    static func logi(
+        _ message: String,
+        file: String = #file,
+        function: String = #function,
+        line: Int = #line
+    ) {
+        logger.info("\(file.components(separatedBy: "/").last ?? "")(\(line)) \(function): \(message)")
+    }
+    
+    static func loge(
+        _ message: String,
+        file: String = #file,
+        function: String = #function,
+        line: Int = #line
+    ) {
+        logger.error("\(file.components(separatedBy: "/").last ?? "")(\(line)) \(function): \(message)\n\(Thread.callStackSymbols.prefix(10).joined(separator: "\n"))")
+    }
+}
