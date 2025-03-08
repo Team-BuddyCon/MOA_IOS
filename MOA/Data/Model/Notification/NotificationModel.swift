ba4ce1ccae7e72f529909cbcf9a42b08a5ca9b1b
ows3090 <ows3090@naver.com>
2025-03-08 17:34:38 +0900

    #167 알림 정보 저장하는 Manager 클래스 구현

---
+++ MOA/Data/Model/Notification/NotificationModel.swift
@@ -0,0 +1,14 @@
+//
+//  NotificationModel.swift
+//  MOA
+//
+//  Created by 오원석 on 3/8/25.
+//
+
+import Foundation
+
+struct NotificationModel {
+    let count: Int
+    let date: String
+    let message: String
+}
