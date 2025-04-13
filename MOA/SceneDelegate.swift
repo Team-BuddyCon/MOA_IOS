fd0e37ec901064e447a85734b1f33bf710b3a517
ows3090 <ows3090@naver.com>
2025-04-13 16:37:42 +0900

    #198 NavigationBar 색상 scroll하면 변경되는 이슈 수정

--- MOA/SceneDelegate.swift
+++ MOA/SceneDelegate.swift
@@ -24,6 +24,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = SplashViewController()
+        window?.backgroundColor = .white
         window?.makeKeyAndVisible()
         
         var rootViewController = UIViewController()
