50f4fc07131014d3c1c4633eb14356ebb4b0fbb5
ows3090 <ows3090@naver.com>
2025-08-16 03:40:28 +0900

    #209 OCR 추출한 텍스트를 통해 사용처, 유효기간 입력하도록 구현

---
+++ MOA/Utils/Regex+.swift
@@ -0,0 +1,16 @@
+//
+//  RegularExpression+.swift
+//  MOA
+//
+//  Created by 오원석 on 8/15/25.
+//
+
+import Foundation
+
+extension NSRegularExpression {
+    static func extractDate() -> String {
+        let datePattern = /\d{4}\.\d{2}\.\d{2}/
+        
+        return ""
+    }
+}
